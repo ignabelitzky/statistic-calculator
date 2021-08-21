3c6b25a78cfcda00316565ee87e213f5035d4765
Ignacio Belitzky <ignabelitzky@gmail.com>
2021-08-21 12:18:40 -0300

    Implemented the save of data via the 'save data' button

--- statistics-calculator/mainwindow.cpp
+++ statistics-calculator/mainwindow.cpp
@@ -20,13 +20,45 @@ MainWindow::MainWindow(QWidget *parent)
     countDataLabel = MainWindow::findChild<QLabel *>(QString("count_label"));
     countDataDisplay = MainWindow::findChild<QLCDNumber *>(QString("countDisplay_lcd"));
 
-    inputDataLineEdit->setText(QString("0.0"));
-    countDataDisplay->display(static_cast<int>(m_data->size()));
+    // Setting the objects to a default value
+    update_data_counter();
+    set_input_display_to_default_value();
+
+    // Connecting SIGNALS and SLOTS
+    connect(saveDataButton, SIGNAL(clicked(bool)), this, SLOT(save_data()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
     delete m_data;
+}
+
+void MainWindow::save_data()
+{
+    QString inputData = inputDataLineEdit->text();
+    if(inputData.isEmpty()) {
+        QMessageBox msgWarning;
+        msgWarning.setText("WARNING!\nYou don't enter any value.");
+        msgWarning.setIcon(QMessageBox::Warning);
+        msgWarning.setWindowTitle("Caution");
+        msgWarning.exec();
+    } else {
+        float value = inputData.toFloat();
+        m_data->push_back(value);
+        std::sort(m_data->begin(), m_data->end());
+        update_data_counter();
+        set_input_display_to_default_value();
+    }
+}
+
+void MainWindow::update_data_counter()
+{
+    countDataDisplay->display(static_cast<int>(m_data->size()));
+}
+
+void MainWindow::set_input_display_to_default_value()
+{
+    inputDataLineEdit->setText(QString("0.0"));
 }
 
--- statistics-calculator/mainwindow.h
+++ statistics-calculator/mainwindow.h
@@ -6,7 +6,9 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QLCDNumber>
+#include <QMessageBox>
 #include <vector>
+#include <algorithm>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -20,6 +22,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void save_data();
+
 private:
     Ui::MainWindow *ui;
     QLabel *titleLabel;
@@ -28,11 +33,16 @@ private:
     QLineEdit *inputDataLineEdit;
     QPushButton *saveDataButton;
     QLCDNumber *countDataDisplay;
+
     std::vector<float> *m_data = nullptr;
+
     float m_arithmeticMean;
     float m_median;
     float m_maximum;
     float m_minimum;
+
+    void update_data_counter();
+    void set_input_display_to_default_value();
 
 };
 #endif // MAINWINDOW_H
